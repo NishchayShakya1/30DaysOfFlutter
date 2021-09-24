33dd461ca95942ba335bcec5bc1602755ba20188
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2021-09-24 13:00:45 +0530

    Day1&2 completed

--- helloflutter/lib/home_page.dart
+++ helloflutter/lib/home_page.dart
@@ -7,7 +7,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
       body: Center(
         child: Container(
           child: Text("Welcome to $days days of $name"),
