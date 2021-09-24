0934909023ac5b29179d89dc88ca77c7f4558af7
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2021-09-24 18:22:58 +0530

    Day 3

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:helloflutter/pages/home_page.dart';
+import 'package:helloflutter/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme:
           ThemeData(brightness: Brightness.dark, primarySwatch: Colors.red),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
