83201621d824b9de5e172f9def84002fec6cf891
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2021-09-26 23:25:28 +0530

    Day-5 Completed

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:helloflutter/pages/home_page.dart';
 import 'package:helloflutter/pages/login_page.dart';
+import 'package:helloflutter/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,16 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        primaryTextTheme: GoogleFonts.latoTextTheme()
-        ),
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
       darkTheme:
           ThemeData(brightness: Brightness.dark, primarySwatch: Colors.red),
-       initialRoute: "/login",
+      initialRoute: "/login",
       routes: {
-        "/": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
