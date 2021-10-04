9cf7b3ed76dd6d0552662b45b9ee33a0ee4d91bb
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2021-10-04 23:24:56 +0530

    Day-12 Completed

--- helloflutter/lib/pages/home_page.dart
+++ helloflutter/lib/pages/home_page.dart
@@ -22,26 +22,31 @@ class _HomePageState extends State<HomePage> {
     loadData();
   }
 
-  loadData()async {
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     var catalogJson = await rootBundle.loadString("assets/files/file.json");
     var decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
-
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-   // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(title: Text("Catalog App")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
+        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
           itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatalogModel.items[index]);
-          },
-        ),
+          itemBuilder: (context, index) =>
+          ItemWidget(item: CatalogModel.items[index])
+          
+        )
+        :Center(child: CircularProgressIndicator(),),
       ),
       drawer: MyDrawer(),
     );
